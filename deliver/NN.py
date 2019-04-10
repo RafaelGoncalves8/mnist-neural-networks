09f35d7ba8549ea010e74c4f6be041d24f06a639
Rafael Goncalves <rafaelgoncalves@riseup.net>
2019-04-09 23:09:06 -0300

    Create mlp model.

--- deliver/NN.py
+++ deliver/NN.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[18]:
+# In[62]:
 
 
 import torch
 import torchvision
 import torchvision.datasets as datasets
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+torch.random.seed = 42
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,7 +20,7 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 
 # ## Load the MNIST dataset
 
-# In[20]:
+# In[63]:
 
 
 mnist_trainset = datasets.MNIST(root='../data', train=True, download=True,
@@ -25,7 +30,7 @@ mnist_testset = datasets.MNIST(root='../data', train=False, download=True,
                                transform=None)
 
 
-# In[21]:
+# In[64]:
 
 
 print(mnist_trainset)
@@ -33,7 +38,14 @@ print('')
 print(mnist_testset)
 
 
-# In[53]:
+# In[74]:
+
+
+M = len(mnist_trainset.data[0])
+M
+
+
+# In[65]:
 
 
 fig = plt.figure(1)
@@ -46,6 +58,67 @@ plt.show()
 
 
 # ## Net architecture
+
+# In[66]:
+
+
+class Net(nn.Module):
+    
+    def __init__(self, M, H, C):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(N, H)
+        self.fc2 = nn.Linear(H, H)
+        self.fc3 = nn.Linear(H, H)
+        self.fc4 = nn.Linear(H, C)
+        self.fc5 = nn.Linear(C, 1)
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax()
+        
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
+        x = self.relu(self.fc3(x))
+        x = self.relu(self.fc4(x))
+        x = self.softmax(self.fc5(x))
+        return x
+
+
+# In[76]:
+
+
+model = Net(M, 10, 10)
+print(model)
+
+
+# In[78]:
+
+
+alpha = 0.1
+
+
+# In[80]:
+
+
+optimizer = optim.SGD(model.parameters(), lr=alpha)
+
+criterion = torch.nn.CrossEntropyLoss()
+
+
+# In[81]:
+
+
+x = mnist_trainset.data
+
+
+# In[82]:
+
+
+optimizer.zero_grad()
+output = model(x)
+loss = criterion(output, target)
+loss.backward()
+optimizer.step()
+
 
 # In[ ]:
 
